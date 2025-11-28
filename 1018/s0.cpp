a67f1ad55f2eabd2ec44db9ec66125c9203c202b
Luiz Barcelos <luizbarcel@gmail.com>
2025-11-28 01:00:31 -0300

    Add solution for problem 1018

--- 1018/s0.cpp
+++ 1018/s0.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <vector>
 
+/*
+- Example 1:
+
+Input: nums = [0,1,1]
+Output: [true,false,false]
+Explanation: The input numbers in binary are 0, 01, 011; which are 0, 1, and 3 in base-10.
+Only the first number is divisible by 5, so answer[0] is true.
+
+- Example 2:
+
+Input: nums = [1,1,1]
+Output: [false,false,false]
+
+*/
+
+
 class Solution {
 public:
-    vector<bool> prefixesDivBy5(vector<int>& nums) {
+    std::vector<bool> prefixesDivBy5(std::vector<int>& nums) {
+        int size = nums.size();
         std::vector<bool> result;
-        int currentNumber = nums[0];
+        int currentRemainder = 0;
 
-        if (nums[0] == 0)
+        for (int i = 0; i < size; ++i)
         {
-            result.push_back(true);
-        }
-        else
-        {
-            result.push_back(false);
-        }
+            if (i == 0)
+            {
+                currentRemainder = nums[i] % 5;
+            }
+            else
+            {
+                currentRemainder = ((currentRemainder << 1) + nums[i]) % 5;
+            }
 
-        int currentPower = 2;
-
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            currentNumber += currentPower * nums[i];
-            std::cout << currentNumber << std::endl;
-            bool currentResult = currentNumber % 5 == 0 ? true : false;
-            result.push_back(currentResult);
-            currentPower *= 2;
+            result.push_back(currentRemainder == 0);
         }
 
         return (result);
@@ -34,14 +45,17 @@ public:
 int main(void) {
     Solution sol;
 
-    std::vector<int> input = {-1,2,9};
-    int k = 2;
+    // std::vector<int> input = {0, 1, 1};
+    // std::vector<int> input = {1, 1, 1};
 
-    // std::vector<int> input = {4,  5,  0, -2, -3,  1};
-    // int k = 5;
+    std::vector<int> input = {1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,1,0};
 
-    int result = sol.subarraysDivByK(input, k);
-    std::cout << result << std::endl;
+    std::vector<bool> result = sol.prefixesDivBy5(input);
+    
+    for (auto elem : result)
+    {
+        std::cout << elem << std::endl;
+    }
 
     return(0);
 }
